aa433cf3d6020e863f23e2cc0ce1884a2d4a9652
Will Jutsum <wjutsum@googlemail.com>
2014-06-14 02:43:02 +0100

    Added Process Category Assignment & Test

--- src/pst/db.py
+++ src/pst/db.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import copy
+
 import os
 import sys
 import datetime
+import traceback
 
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, and_
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, and_, update
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -19,6 +24,7 @@ Base = declarative_base()
 class DBConnection():
     def __init__(self, db_filename='pst.db'):
         self.engine = create_engine('sqlite:///' + db_filename)
+
         self.engine.raw_connection().connection.text_factory = str
         Base.metadata.create_all(self.engine)
         DBSession = sessionmaker(bind=self.engine)
@@ -30,27 +36,35 @@ class DBConnection():
             self.session.rollback()
 
     def add_process(self, process):
-
+        now = datetime.datetime.now()
         processtoadd = DBProcess(filename=process.filename,
                                  process_id=process.id,
                                  title=process.title,
                                  session=self.session,
-                                 start_time=datetime.datetime.now(),
-                                 end_time=datetime.datetime.now())
+                                 start_time=now,
+                                 end_time=now)
         try:
             if (self.current_process.filename == processtoadd.filename and
                 self.current_process.title == processtoadd.title):
-                self.current_process.end_time = datetime.datetime.now()
+
+                self.current_process.end_time = now
+                self.session.expunge(processtoadd)
+                # processtoadd = None
                 self.session.commit()
-                print ("Updated Process")
+
                 return self.current_process
+        except Exception, e:
+            # print(repr(traceback.format_exc()))
+            pass
+        # print ("Adding Process")
+        try:
+            self.current_process.end_time = now
         except:
             pass
-        print ("Adding Process")
         self.session.add(processtoadd)
         self.session.commit()
         self.current_process = processtoadd
-        print ("Added Process")
+        # print ("Added Process")
         return processtoadd
 
     def add_screenshot(self, screenshot_id, screenshot_path):
@@ -61,17 +75,29 @@ class DBConnection():
         self.session.commit()
         return screenshot_to_add
 
+    def add_category(self,title,title_search,filename_search):
+        new_cat = ProcessCategory(title=title,
+                                  title_search=title_search,
+                                  filename_search=filename_search)
+        self.session.add(new_cat)
+        self.session.commit()
+        return new_cat
+
     def get_screenshots(self):
         data = self.session.query(Screenshot).limit(100)
         return data
 
-    def get_processes(self,start_time,end_time):
+    def get_processes(self,start_time=None,end_time=None):
+        if start_time is None:
+            start_time = 0
+        if end_time is None:
+            end_time = datetime.datetime.now()
         # data = self.session.query(DBProcess,ProcessType,ProcessCategory).order_by(DBProcess.datetime.desc()).limit(100)
         data = self.session.query(DBProcess,ProcessType,ProcessCategory)\
             .join(ProcessType)\
             .join(ProcessCategory)\
             .filter(and_(DBProcess.start_time <=end_time,DBProcess.start_time>=start_time))\
-            .order_by(DBProcess.start_time.desc())\
+            # .order_by(DBProcess.start_time.desc())\
             # .limit(100)
         # data = self.session.query(DBProcess,ProcessType).join(ProcessType).limit(10)
         return data
@@ -81,10 +107,26 @@ class DBConnection():
         self.session.add(process_category)
         self.session.commit()
 
+    def assign_categories(self):
+        categories = self.session.query(ProcessCategory)
+        process_types = self.session.query(ProcessType)
+        for category in categories:
+            for process_type in process_types:
+                if category.title_search.lower() in process_type.title.lower() and \
+                    category.filename_search.lower() in process_type.filepath.lower():
+                    process_type.process_category = category
+
+
+        self.session.commit()
+
+
+
 class ProcessCategory(Base):
     __tablename__ = 'process_category'
     id = Column(Integer, primary_key=True)
     title = Column(String(250), nullable=False)
+    title_search = Column(String(250), default='')
+    filename_search = Column(String(250), default='')
     def __repr__(self):
         return "<ProcessCategory('%s')>" % self.title
 
@@ -92,6 +134,7 @@ class ProcessType(Base):
     __tablename__ = 'process_type'
     id = Column(Integer, primary_key=True)
     filepath = Column(String(250), nullable=False)
+    title = Column(String(250,convert_unicode=True), nullable=False)
     process_category_id = Column(Integer, ForeignKey(ProcessCategory.id),default=0)
     process_category = relationship(ProcessCategory)
     def __repr__(self):
@@ -103,7 +146,7 @@ class DBProcess(Base):
     id = Column(Integer, primary_key=True)
     filename = Column(String(250), nullable=False)
     process_id = Column(Integer, nullable=False)
-    title = Column(String(250), nullable=False)
+    title = Column(String(250,convert_unicode=True), nullable=False)
     start_time = Column(DateTime, nullable=False)
     end_time = Column(DateTime, nullable=False)
     process_type_id = Column(Integer, ForeignKey(ProcessType.id))
@@ -112,14 +155,15 @@ class DBProcess(Base):
     def __init__(self, filename, process_id, title, session, start_time,end_time):
         self.filename = filename
         self.process_id = process_id
-        self.title = title
+        print("TITLE: "+title)
+        self.title = title.encode('string_escape')
         self.start_time = start_time
         self.end_time = end_time
         try:
             process_type = session.query(ProcessType) \
-                .filter(ProcessType.filepath == filename).one()
+                .filter(ProcessType.filepath == filename, ProcessType.title == self.title).one()
         except InvalidRequestError:
-            process_type = ProcessType(filepath=filename)
+            process_type = ProcessType(filepath=filename,title=self.title)
             session.add(process_type)
         self.process_type = process_type
         self.process_type_id = process_type.id
--- src/test/test_processes.py
+++ src/test/test_processes.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import json
 import unittest
 import os
@@ -11,7 +13,7 @@ import pst.Process
 import pst.screenshots
 import pst.db
 from pst.db import DBConnection
-from pst.helpers import rm
+from pst.helpers import rm,random_string
 
 import pst.WebServer
 from pst.DataService import DataService
@@ -64,22 +66,20 @@ class TestDataBase(unittest.TestCase):
         db.session.close()
 
     def test_save_and_update_process(self):
-        testdb = str(time.time())+".db"
-        # testdb = "test.db"
+        # testdb = str(time.time())+".db"
+        testdb = "test.db"
         current_process = pst.processes.get_current()
+        current_process.title = "TITLE: Unicode HOWTO � Python v2.7.7 documentation - Google Chrome"
         db = DBConnection(db_filename=testdb)
         process_start = db.add_process(current_process)
         time.sleep(0.5)
         second_current_process = pst.processes.get_current()
         process_end = db.add_process(current_process)
+        process_end.title = "TITLE: Unicode HOWTO � Python v2.7.7 documentation - Google Chrome"
         self.assertEquals(process_start.id,process_end.id);
-        self.assertNotEquals(process_start.start_time,process_end.end_time);
+        self.assertNotEquals(process_end.start_time,process_end.end_time);
         db.session.close()
-        rm(testdb)
-
-
-
-
+        # rm(testdb)
     def test_save_screenshot(self):
         testdb = str(time.time()) + ".db"
         db = DBConnection(db_filename=testdb)
@@ -136,6 +136,39 @@ class TestDataBase(unittest.TestCase):
         db = DBConnection(db_filename=testdb)
         pc = db.session.query(pst.db.ProcessCategory).one()
         self.assertTrue(pc.id == 0)
+        db.session.close()
+        rm(testdb)
+
+
+    def test_add_process_category(self):
+        testdb = str(time.time()) + ".db"
+        db = DBConnection(db_filename=testdb)
+        new_category = db.add_category(title="TITLE",title_search = "test",filename_search = "test")
+        self.assertEquals(new_category.id,1)
+        pc = db.session.query(pst.db.ProcessCategory).filter(pst.db.ProcessCategory.title == "TITLE").one()
+        self.assertEquals(pc.id,1)
+        db.session.close()
+        rm(testdb)
+
+    def test_add_process_and_process_category_and_assign(self):
+        testdb = str(time.time()) + ".db"
+        db = DBConnection(db_filename=testdb)
+        for x in xrange(0,5):
+            current_process = pst.processes.get_current()
+            random_title = random_string(10)
+            random_filename = random_string(10)
+            current_process.title = "TEST TITLE " + random_title
+            current_process.filename = "TEST FILENAME " + random_filename
+            process = db.add_process(current_process)
+            new_category = db.add_category(title="TITLE",title_search = random_title,filename_search = random_filename)
+            db.assign_categories()
+            # processes = db.get_processes()
+            processes = [pst.db.row2dict(row) for row in db.get_processes()]
+            self.assertEqual(new_category.id,int(processes[x]['process_category']['id']))
+        db.session.close()
+        rm(testdb)
+
+
 
 
 class TestScheduler(unittest.TestCase):
